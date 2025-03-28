9b619270d7b1f57d5be9958e25929b183f70d5e6
Ze Peng <zhpeng@ualberta.ca>
2025-03-27 22:49:07 -0600

    deploy to github pages (#1)

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: process.env.NODE_ENV === 'production' ? '/SuperBrainGameHub' : '',
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
